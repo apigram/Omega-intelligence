c8da449376df191a5a6dfafbd9231dfcd770a658
pi <pi@raspberrypi>
2016-05-05 20:48:27 +1000

    Initial project revision.

---
+++ Omega Intelligence/Tree.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   Tree.cpp
+ * Author: pi
+ * 
+ * Created on 5 May 2016, 8:14 PM
+ */
+
+#include "Tree.h"
+
+Tree::Tree() {
+}
+
+Tree::Tree(const Tree& orig) {
+}
+
+Tree::~Tree() {
+}
+
